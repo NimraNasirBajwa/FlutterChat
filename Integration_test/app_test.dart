02f8aec2faf5b77f88fd407bf9878e5a2a61237c
NimraNasir <nimra.nasir@vdotok.com>
2022-12-06 18:16:57 +0500

    new

---
+++ Integration_test/app_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:vdkFlutterChat/main.dart' as app;
+void main() {
+  group('App Test', () {
+    IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+    testWidgets('Full app Test', (tester) async {
+    app.main();
+    await tester.pumpAndSettle();
+
+    final emailFromField = find.byType(TextFormField).first;
+    final passwordFromField = find.byType(TextFormField).last;
+    final signInButton = find.byType(ElevatedButton).first;
+
+    await tester.enterText(emailFromField, 'Nimra');
+    await tester.enterText(passwordFromField, 'Nimra123');
+    await tester.tap(signInButton);
+    await tester.pumpAndSettle();
+    });
+  });
+}
